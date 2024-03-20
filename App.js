dc93e0bc7e678ffb70f8f5fef9790164eda965ad
jcameron84 <joelcameron0406@gmail.com>
2024-03-20 11:44:21 +1000

    Moved files into git repo

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!!</Text>
+      <Text>_____________</Text>
+      <Text>From Group 36</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
