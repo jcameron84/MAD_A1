a75bf1e77360fc039b627dca96819a1f893ea68e
jcameron84 <joelcameron0406@gmail.com>
2024-03-30 17:33:29 +1000

    Added js files to screens folder
    made AddToDo.js and Home.js and removed "Home" code from app.js

---
+++ src/screens/addToDo.js
@@ -0,0 +1,65 @@
+import { StatusBar } from 'expo-status-bar';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+export default function AddToDo() {
+  return (
+    <View style={{padding: 20, backgroundColor: '#292929', alignItems: 'center', flex: 1, justifyContent: 'top'}}>
+    <View style={{padding: 20, width: '100%', height: '100%', borderWidth: 3, borderRadius: 10, flexDirection: 'column', flex: 1, borderColor: 'white',justifyContent: 'top'}}>
+      <View style={{width: '95%', height: 50, borderWidth: 3, borderRadius: 10, flexDirection: 'row', borderColor: 'white',justifyContent: 'center'}}>
+            <Text style={{fontSize: 30, color: 'white'}}>Add New To-Do</Text>
+      </View>
+      <View style={{height: 50}}></View>
+
+      
+
+      <View style={{height: '65%'}}></View>
+
+      <View style={[styles.listItem, {justifyContent: 'center'}]}>
+        <Pressable
+          //style={getButStyle}
+          onPress={() => {
+            console.log('But Pressed');
+          }}
+        >
+          <Text style= {[{color:'white', fontSize: 20}]}>+ Add New To-Do</Text>
+        </Pressable>
+      </View>
+
+
+
+      </View>
+
+      
+        
+      
+    </View>
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  listItem: {
+    padding: 4,
+    width: '95%',
+    height: 40,
+    borderWidth: 3,
+    borderRadius: 10,
+    flexDirection: 'row',
+    borderColor: 'white',
+    justifyContent: 'left',
+  },
+
+  listItemText: {
+    fontSize: 20,
+    color: 'white',
+  }
+
+});
